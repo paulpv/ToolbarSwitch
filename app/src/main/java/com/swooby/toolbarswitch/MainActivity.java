bb8817e95203006b5c263548ca4b279572d3c6a4
Paul Peavyhouse <pv@swooby.com>
2015-02-22 09:03:50 -0800

    Adding logging and toast message

--- app/src/main/java/com/swooby/toolbarswitch/MainActivity.java
+++ app/src/main/java/com/swooby/toolbarswitch/MainActivity.java
@@ -3,27 +3,42 @@ package com.swooby.toolbarswitch;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity
         extends ActionBarActivity
 {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        Log.d(TAG, "+onCreate(...)");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.d(TAG, "-onCreate(...)");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "Why is this logged before onCreateOptionsMenu?");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
+        Log.d(TAG, "Why is this logged after onResume?");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // TODO:(pv) How to get a reference to the action_switch Switch control?
         return true;
     }
 
@@ -37,6 +52,8 @@ public class MainActivity
         switch (id)
         {
             case R.id.action_switch:
+                Toast.makeText(this, "action_switch", Toast.LENGTH_LONG).show();
+                // TODO:(pv) Why is item.mActionView null?
                 return true;
             case R.id.action_settings:
                 return true;
