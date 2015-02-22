a002480ecfb6cb42d07b8c754c0469d8be61e9e9
Paul Peavyhouse <pv@swooby.com>
2015-02-22 13:32:48 -0800

    Fixed the problem by changing android:actionLayout to app:actionLayout!

--- app/src/main/java/com/swooby/toolbarswitch/MainActivity.java
+++ app/src/main/java/com/swooby/toolbarswitch/MainActivity.java
@@ -6,51 +6,63 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
-
 public class MainActivity
-        extends ActionBarActivity
-{
+        extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "+onCreate(...)");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
         Log.d(TAG, "-onCreate(...)");
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         Log.d(TAG, "Why is this logged before onCreateOptionsMenu?");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         Log.d(TAG, "Why is this logged after onResume?");
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        // TODO:(pv) How to get a reference to the action_switch Switch control?
+        MenuItem item = menu.findItem(R.id.action_switch);
+        if (item != null) {
+            CharSequence title = item.getTitle();
+            Switch action_bar_switch = (Switch) item.getActionView().findViewById(R.id.action_switch);
+            if (action_bar_switch != null) {
+                action_bar_switch.setText(title);
+                action_bar_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        int resId = isChecked ? R.string.enabled : R.string.disabled;
+                        Toast.makeText(MainActivity.this, getString(resId), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        switch (id)
-        {
+        switch (id) {
             case R.id.action_switch:
                 Toast.makeText(this, "action_switch", Toast.LENGTH_LONG).show();
                 // TODO:(pv) Why is item.mActionView null?
